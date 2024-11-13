6d0effbbdb0323af5aaf3938ae047d41f29a8c66
emerdenny <e@ewd.dev>
2024-11-13 18:45:18 -0500

    add lua-language-server

--- enixNvim/common.nix
+++ enixNvim/common.nix
@@ -1,41 +1,49 @@
-{ pkgs, system, config, lib, home-manger, ... }:
-let
+{
+  pkgs,
+  system,
+  config,
+  lib,
+  home-manger,
+  ...
+}: let
   refresh = pkgs.writeScriptBin "nvim-refresh" ''
     rm -rf ~/.config/nvim/lazy-lock.json
     rm -rf ~/.config/lvim
-    
+
     rm -rf ~/.cache/lvim
     rm -rf ~/.cache/nvim
-    
+
     rm -rf ~/.local/share/lvim
     rm -rf ~/.local/share/nvim
-    
+
     rm -rf ~/.local/state/lvim
     rm -rf ~/.local/state/nvim
   '';
-in
-{
-  imports = [ ./options.nix ];
+in {
+  imports = [./options.nix];
   config = {
-    environment.systemPackages = with pkgs; [
-      # Essential
-      # neovim
-      ripgrep
-      lazygit
-      gdu
-      bottom
-      # LSP dependencies
-      stylua
-      selene
-      ruff
-      config.enixNvim.pythonPackage
-      config.enixNvim.nodePackage
-      rustup # Must run `rustup default stable`
-      nixd
-      alejandra
-      deadnix
-      statix
-    ] ++ [ refresh ];
+    environment.systemPackages = with pkgs;
+      [
+        # Essential
+        # neovim
+        ripgrep
+        lazygit
+        gdu
+        bottom
+        # LSP dependencies
+        stylua
+        selene
+        lua-language-server
+        ruff
+        config.enixNvim.pythonPackage
+        config.enixNvim.nodePackage
+        rustup # Must run `rustup default stable`
+        nixd
+        alejandra
+        deadnix
+        statix
+      ]
+      ++ [refresh];
     home-manager.users.${config.enixNvim.username}.xdg.configFile = {
       "nvim/lua" = {
         source = ../src/lua;
