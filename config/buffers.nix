27f48a5b0fdcd4d427303bad0c644d6206219d2a
emerdenny <e@ewd.dev>
2024-12-11 03:29:56 -0500

    fix buffer keymap actions

--- config/buffers.nix
+++ config/buffers.nix
@@ -5,32 +5,29 @@
   };
 
   keymaps = [
-    # TODO test
     {
       mode = "n";
       key = "<leader>bb";
-      action = "<cmd>BufferLinePick<esc>";
+      action = "<Cmd>BufferLinePick<CR>";
       options.desc = "Pick to goto";
     }
-    # TODO test
     {
       mode = "n";
       key = "<leader>bc";
-      action = "<cmd>BufferLineCloseOthers<esc>";
+      action = "<Cmd>BufferLineCloseOthers<CR>";
       options.desc = "Close all others";
     }
-    # TODO test
     {
       mode = "n";
       key = "<leader>bd";
-      action = "<cmd>BufferLinePickClose<esc>";
+      action = "<Cmd>BufferLinePickClose<CR>";
       options.desc = "Pick to close";
     }
-    # TODO test
+    # TODO implement me
     # {
     #   mode = "n";
     #   key = "<leader>bp";
-    #   action = "<cmd>BufferLineGoToBuffer<esc>";
+    #   action = "<Cmd>BufferLineGoToBuffer<esc>";
     #   options.desc = "Goto last buffer";
     # }
   ];
